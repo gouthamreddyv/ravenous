f1e751efd3a0162386e43753e7e5af0cbcb17284
gouthamvundyala <gouthamreddyv@gmail.com>
2018-11-20 07:05:35 +1100

    static page

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+};
+
+class Searchbar extends
+React.Component {
+  render (){
+
+    return (
+<div className="SearchBar">
+   <div className="SearchBar-sort-options">
+     <ul>
+       renderSortByOptions()
+     </ul>
+   </div>
+   <div className="SearchBar-fields">
+     <input placeholder="Search Businesses" />
+     <input placeholder="Where?" />
+   </div>
+   <div className="SearchBar-submit">
+     <a>Let's Go</a>
+   </div>
+</div>
+    );
+
+  }
+
+   renderSortByOptions (){
+
+    return Object.keys(sortByOptions).map(sortByOption => {
+
+      let sortByOptionValue = sortByOptions[sortByOption];
+        return <li key={sortByOption}> {sortByOptionValue} </li>;
+    });
+
+  }
+}
+
+export default Searchbar;
